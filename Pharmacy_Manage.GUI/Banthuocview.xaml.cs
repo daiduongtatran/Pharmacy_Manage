5d5778e6cba2a7b32bb50a39e4c1399b0a0d9e30
Nguyen Duy Tan <ubadodiaz@example.com>
2026-02-02 09:54:35 +0700

    logic nút menu 4 chức năng cho nhân viên

---
+++ Pharmacy_Manage.GUI/Banthuocview.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pharmacy_Manage.GUI
+{
+    /// <summary>
+    /// Interaction logic for Banthuocview.xaml
+    /// </summary>
+    public partial class Banthuocview : UserControl
+    {
+        public Banthuocview()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Pharmacy_Manage.GUI/StaffWindow.xaml.cs
+++ Pharmacy_Manage.GUI/StaffWindow.xaml.cs
@@ -22,6 +22,51 @@ namespace Pharmacy_Manage.GUI
         public StaffWindow()
         {
             InitializeComponent();
+            MainContent.Content = banThuocView;
+        }
+        private Banthuocview banThuocView = new();
+        private QLthuoc_view qLthuoc_View = new(); 
+        private QLkhachhang_view quanLyKhachView = new();
+
+        private void ResetMenuHighlight()
+        {
+            BtnBanthuoc.Background = Brushes.Transparent;
+            BtnQLthuoc.Background = Brushes.Transparent;
+            BtnQLkhachhang.Background = Brushes.Transparent;
+        }
+
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            ResetMenuHighlight();
+
+            Button btn = sender as Button;
+            btn.Background = new SolidColorBrush(Color.FromRgb(52, 152, 219));
+
+            int tag = int.Parse(btn.Tag.ToString());
+
+            switch (tag)
+            {
+                case 0:
+                    MainContent.Content = banThuocView;
+                    break;
+                case 1:
+                    MainContent.Content = qLthuoc_View;
+                    break;
+                case 2:
+                    MainContent.Content = quanLyKhachView;
+                    break;
+            }
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn đăng xuất không?", "Xác nhận", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                MainWindow login = new MainWindow();
+                login.Show();
+                this.Close();
+            }
         }
     }
 }
